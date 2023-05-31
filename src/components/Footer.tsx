fa68e757a11a7f62b6fdd298cd65effd9401941d
Owlbeard <omeraltun96@gmail.com>
2023-05-31 20:49:46 +0300

    Add link to footer logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,9 @@ import GitHub from '../assets/github.svg';
 export default function Footer() {
   return (
     <div className="container flex justify-center items-center gap-4">
-      <img className="sm:h-12 h-8" src={GitHub} alt="Github Logo" />
+      <a href="https://github.com/owlbeard">
+        <img className="sm:h-12 h-8" src={GitHub} alt="Github Logo" />
+      </a>
       <p className="sm:text-lg text-xs">Copyright © 2023 || owlbeard</p>
     </div>
   );
